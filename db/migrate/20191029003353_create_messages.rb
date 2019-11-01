752dd39aac22067d522dabff9bd5debc952f1d60
Junkey-493 <56882148+Junkey-493@users.noreply.github.com>
2019-11-01 10:14:55 +0900

    create user table

--- db/migrate/20191029003353_create_messages.rb
+++
@@ -1,9 +0,0 @@
-class CreateMessages < ActiveRecord::Migration[5.0]
-  def change
-    create_table :messages do |t|
-      t.string :content, null: false
-      t.text :image
-      t.timestamps
-    end
-  end
-end
